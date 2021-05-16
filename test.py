bc24c898ca84f469d1ccbb0e8a5280441322e280
michelemaresca <michele.maresca1998@gmail.com>
2021-05-16 15:36:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,27 @@
 from anneal import SimAnneal
-import matplotlib.pyplot as plt
-import random
 
 
 def read_coords(path):
     coords = []
     with open(path, "r") as f:
-        for i in range(0,3):
+        for i in range(0, 3):
                 line = f.readline()
         line = f.readline()
         line = line.strip('\n')
         data = line.split(':')
         dimension = int(data[1])
-        for i in range(0,2):
+        for i in range(0, 2):
             line = f.readline()
         for i in range(0, dimension):
             line = f.readline()
             data = line.split()
-            coord = [float(data[1]),float(data[2])]
+            coord = [float(data[1]), float(data[2])]
             coords.append(coord)
     return coords, dimension
 
+
 if __name__ == "__main__":
-    coords,dimension = read_coords("a280.txt")
+    coords, dimension = read_coords("a280.txt")
     #print(coords)
     sa = SimAnneal(coords, stopping_iter=5000)
     sa.anneal()
