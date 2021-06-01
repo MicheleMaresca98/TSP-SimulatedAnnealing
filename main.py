c2362365a86728c5dfe24ffb9ba239b6edd79c3a
michelemaresca <michele.maresca1998@gmail.com>
2021-06-01 12:49:08 +0200

    Prova nuova greedy

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     indice_inizio_linee_coordinate = 9
     coordinate, dimensione = leggi_coordinate("IstanzeBenchmark/burma14.txt", indice_linea_dimensione,
                                               indice_inizio_linee_coordinate)
-    b = 554070.556050  # 19947008.20167722 #239297 #2579 #3323  # best solution istanza benchmark
+    b = 33.23 # 554070.556050  # 19947008.20167722 #239297 #2579  # best solution istanza benchmark
     tsp = TSP(coordinate)
     eur = tsp.simulatedannealing()
     gap = (abs(float(b) - float(eur)) / abs(float(b))) * 100.0
+    print(gap)
     # tsp.visualizza_tour()
