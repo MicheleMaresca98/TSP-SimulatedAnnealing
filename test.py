1aaff15d291349171dc48f16de310d6bd37882d1
Vingiu99 <vincenzo.riccardi4@studenti.unina.it>
2021-06-03 13:26:50 +0200

    Cambiamenti nomi file Gready -> Greedy e pulizia codice

--- test.py
+++ test.py
@@ -5,33 +5,34 @@ from leggi_coordinate import leggi_coordinate
 if __name__ == "__main__":
     indice_linea_dimensione = 4
     indice_inizio_linee_coordinate = 7
-    coordinate, dimensione = leggi_coordinate("IstanzeBenchmark/vm1084.tsp.txt", indice_linea_dimensione, indice_inizio_linee_coordinate)
+    coordinate, dimensione = leggi_coordinate("IstanzeBenchmark/rl5934.tsp.txt", indice_linea_dimensione, indice_inizio_linee_coordinate)
     b = 554070.556050  # 19947008.20167722 #239297 #2579 #3323  # best solution istanza benchmark
     L = 280
-    t_in = [10**i for i in range(1,11)]
+    t_in = 100
     t_fin = 1
     alpha = 0.99
     print("vm1804\n")
-    with open("Test/nearest_neighbour_vm1084/test_ordini_di_grandezza_vm1084_v1.txt", 'w') as f:
+    with open("Test/Test_grandi_dimesioni/test_grandi_dimensioni_paametri_ottimali.txt", 'w') as f:
         f.write('test for nearest_neighbour_vm1084_v1.txt : \n')
         print("primo: \n")
-        for i in range(0,10):
-            tsp = TSP(coordinate,t_in[i],t_fin,alpha,L)
-            eur = tsp.simulatedannealing()
-            f.write(str(i+1))
-            f.write(') temperatura inziale : ')
-            f.write(str(t_in[i]))
-            f.write('\n temperatura finale : ')
-            f.write(str(t_fin))
-            f.write('\n alpha :')
-            f.write(str(alpha))
-            f.write('\n iterazioni alla stessa temperatura : ')
-            f.write(str(L))
-            f.write('\n Esito euristica :')
-            f.write(str(eur))
-            f.write('\n')
-            print(i,"\n")
+        tsp = TSP(coordinate,t_in[i],t_fin,alpha,L)
+        eur = tsp.simulatedannealing()
+        f.write(str(i+1))
+        f.write(') temperatura inziale : ')
+        f.write(str(t_in[i]))
+        f.write('\n temperatura finale : ')
+        f.write(str(t_fin))
+        f.write('\n alpha :')
+        f.write(str(alpha))
+        f.write('\n iterazioni alla stessa temperatura : ')
+        f.write(str(L))
+        f.write('\n Esito euristica :')
+        f.write(str(eur))
+        f.write('\n')
+        print(i,"\n")
 
+
+'''''''''''''''''''''''''''''
     with open("Test/nearest_neighbour_vm1084/test_ordini_di_grandezza_vm1084_v2.txt", 'w') as f:
         f.write('test for nearest_neighbour_vm1084_v2.txt : \n')
         print("secondo: \n")
@@ -142,3 +143,5 @@ if __name__ == "__main__":
             f.write(str(eur))
             f.write('\n')
             print(i, "\n")
+            
+'''''''''''''''''''''''''''''''''
--- test_random_vs_greedy.py
+++ test_random_vs_greedy.py
@@ -6,38 +6,48 @@ from leggi_coordinate import leggi_coordinate
 
 if __name__ == "__main__":
     nuovo_file = openpyxl.Workbook()
+    L = 280
+    t_in = 100
+    t_fin = 1
+    alpha = 0.99
     indice_linea_dimensione = 4
-    indice_inizio_linee_coordinate = 9
-    coordinate, dimensione = leggi_coordinate("IstanzeBenchmark/burma14.txt", indice_linea_dimensione, indice_inizio_linee_coordinate)
-    sheet = nuovo_file.create_sheet('burma')
-    tsp = TSP(coordinate)
-    tsp_random = TSP_RANDOM(coordinate)
-    obj_finale,obj_iniziale = tsp.simulatedannealing()
-    obj_finale_random,obj_iniziale_random = tsp_random.simulatedannealing()
-    sheet['A1'] = 'burma14'
+    indice_inizio_linee_coordinate = 7
+    coordinate_rl, dimensione_rl = leggi_coordinate("IstanzeBenchmark/rl5934.tsp.txt", indice_linea_dimensione, indice_inizio_linee_coordinate)
+    coordinate_usa, dimensione_usa = leggi_coordinate("IstanzeBenchmark/usa13509.tsp.txt", 7,10)
+    sheet = nuovo_file.create_sheet('instanze_grandi')
+    tsp_rl = TSP(coordinate_rl,t_in,t_fin,alpha,L)
+    tsp_usa = TSP(coordinate_usa,t_in,t_fin,alpha,L)
+    obj_finale_rl = tsp_rl.simulatedannealing()
+    print("rl5934")
+    obj_finale_usa= tsp_usa.simulatedannealing()
+    print("usa13509")
+    sheet['A1'] = 'istanze_grandi'
     sheet['A2'] = 'L'
     sheet['A3'] = 'alpha'
     sheet['A4'] = 'temp_iniz'
     sheet['A5'] = 'temp_finale'
-    sheet['A6'] = 'obj_sol_in'
-    sheet['A7'] = 'obj_sol_fin'
-    sheet['B1'] = 'random'
-    sheet['B2'] = tsp_random.N
-    sheet['B3'] = tsp_random.alpha
-    sheet['B4'] = tsp_random.temperatura_iniziale
-    sheet['B5'] = tsp_random.temperatura_finale
-    sheet['B6'] = obj_iniziale_random
-    sheet['B7'] = obj_finale_random
-    sheet['C1'] = 'greedy'
-    sheet['C2'] = tsp.N
-    sheet['C3'] = tsp.alpha
-    sheet['C4'] = tsp.temperatura_iniziale
-    sheet['C5'] = tsp.temperatura_finale
-    sheet['C6'] = obj_iniziale
-    sheet['C7'] = obj_finale
-    print('burma14')
+    sheet['A6'] = 'obj_sol_fin'
+    sheet['A7'] = 'best_solution'
+    sheet['A8'] = 'gap'
+    sheet['B1'] = 'rl5934'
+    sheet['B2'] = L
+    sheet['B3'] = alpha
+    sheet['B4'] = t_in
+    sheet['B5'] = t_fin
+    sheet['B6'] = obj_finale_rl
+    sheet['B7'] = 554070
+    sheet['B8'] = (obj_finale_rl-554070)/554070
+    sheet['C1'] = 'usa13509'
+    sheet['C2'] = L
+    sheet['C3'] = alpha
+    sheet['C4'] = t_in
+    sheet['C5'] = t_fin
+    sheet['C6'] = obj_finale_usa
+    sheet['C7'] = 19947008
+    sheet['C8'] = (obj_finale_usa-19947008)/19947008
+    print('fine')
 
-
+'''''''''''
     indice_linea_dimensione = 4
     indice_inizio_linee_coordinate = 7
     coordinate, dimensione = leggi_coordinate("IstanzeBenchmark/berlin52.tsp", indice_linea_dimensione, indice_inizio_linee_coordinate)
@@ -134,4 +144,4 @@ if __name__ == "__main__":
 
     nuovo_file.save('Test/Random_VS_Gready_standard_parameter/Random_vs_Gready.xlsx')
 
-
+'''''
